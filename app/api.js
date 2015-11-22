530027e3bf996fc439a40232205e6f5979ca9ea8
Miriam Shiffman <meereeum@gmail.com>
2015-11-22 11:17:44 -0500

    safe encode urls

--- app/api.js
+++ app/api.js
@@ -1,8 +1,8 @@
 
 export default {
-    uri: 'https://paleobiodb.org/data1.1/colls/summary.json?level=2&interval=',
+    uri: 'https://paleobiodb.org/data1.2/colls/summary.json?level=2&interval=',
     getInterval(interval){
-        return fetch(this.uri + interval)
+        return fetch(this.uri + encodeURIComponent(interval))
             .then((response) => {
                 if(!response.ok){
                     throw 'bad response'

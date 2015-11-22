44771e682d196cc43b6f57ca19de6d8a7b2451c8
Sher Minn Chong <sherminn.chong@gmail.com>
2015-11-22 04:30:58 -0500

    Add tooltips

--- app/index.js
+++ app/index.js
@@ -1,55 +1,54 @@
-import api from './api'
-import 'styles/style.scss' 
-import d3 from 'd3'
-// import topojsons from './topojson'
-import _ from 'lodash'
-import topojsons from './geojson'
+import api from './api';
+import 'styles/style.scss';
+import d3 from 'd3';
 
-let RECORDS = {}
+// import worlds from './topojson'
+import _ from 'lodash';
+import worlds from './geojson';
 
-api.getInterval('Jurassic').then((data)=>{
-    addRecords(data.records)
+let RECORDS = {};
+
+api.getInterval('Jurassic').then((data)=> {
+    addRecords(data.records);
 }).then(()=> {
     api.getInterval('Triassic').then((data) => {
-        addRecords(data.records)
-        drawMap()
-    })
+        addRecords(data.records);
+        drawMap();
+    });
 
-})
+});
 
-function addRecords(records){
-    for(let record of records){
-        if (!record || !('oid' in record)){
+function addRecords(records) {
+    for (let record of records) {
+        if (!record || !('oid' in record)) {
             continue;
         }
 
-        const oid = record.oid
-        if (RECORDS[oid]){
-            RECORDS[oid].push(record)
-        }
-        else {
-            RECORDS[oid] = [record]
-        }
-    }
-}
-
-function findDuplicates(records){
-    for (let oid in records){
-        const record = records[oid]
-        if(record.length > 1){
-            const latlng = record.map((r, i) =>{
-                return `${r.lat}, ${r.lng}`
-            })
-            console.log(latlng)
+        const oid = record.oid;
+        if (RECORDS[oid]) {
+            RECORDS[oid].push(record);
+        } else {
+            RECORDS[oid] = [record];
         }
     }
 }
 
+function findDuplicates(records) {
+    for (let oid in records) {
+        const record = records[oid];
+        if (record.length > 1) {
+            const latlng = record.map((r, i) => {
+                return `${r.lat}, ${r.lng}`;
+            });
+            console.log(latlng);
+        }
+    }
+}
 
-function drawMap(){
+function drawMap() {
     const width = window.innerWidth, height = window.innerHeight;
-    let isRotating = false
-    let mousePos = []
+    let isRotating = false;
+    let mousePos = [];
 
     const lambda = d3.scale.linear()
         .domain([0, width])
@@ -67,56 +66,67 @@ function drawMap(){
     const path = d3.geo.path()
         .projection(projection);
 
-
     const mapMouseMove = () => {
-        if(isRotating){
-            const [x, y] = [d3.event.pageX, d3.event.pageY]
-            projection.rotate([lambda(x), phi(y)])
-            svg.selectAll("path").attr("d", path);
+        if (isRotating) {
+            const [x, y] = [d3.event.pageX, d3.event.pageY];
+            projection.rotate([lambda(x), phi(y)]);
+            svg.selectAll('path').attr('d', path);
         }
-    }
+    };
 
     const mapMouseDown = () => {
-        d3.event.preventDefault()
-        isRotating = true
-    }
+        d3.event.preventDefault();
+        isRotating = true;
+    };
 
     const mapMouseUp = () => {
-        d3.event.preventDefault()
-        isRotating = false
-    }
+        d3.event.preventDefault();
+        isRotating = false;
+    };
 
-
-    var svg = d3.select("#map").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("id", "svgmap")
+    var svg = d3.select('#map').append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('id', 'svgmap')
         .on('mousedown', mapMouseDown)
         .on('mousemove', mapMouseMove)
-        .on('mouseup', mapMouseUp)
+        .on('mouseup', mapMouseUp);
 
+    svg = d3.select('#svgmap')
+        .attr('width', width)
+        .attr('height', height);
 
-    svg = d3.select("#svgmap")
-        .attr("width", width)
-        .attr("height", height);
+    const tooltip = d3.select('body')
+        .append('div')
+        .attr('class', 'map-tooltip')
+        .style('position', 'absolute')
+        .style('z-index', '10')
+        // .style("visibility", "hidden")
+        .style('left', '20px')
+        .style('top', '20px')
+        .text('a simple tooltip');
+
 
     let start;
     let frame = 0;
-    let locked = false
+    let locked = false;
 
-    window.foo = () =>{
-        frame++
-        if(frame < topojsons.length) render(topojsons[topojsons.length - frame - 1], path, svg)
-    }
+    window.foo = () => {
+        frame++;
+        if (frame < worlds.length) {
+            render(worlds[worlds.length - frame - 1], path, svg, tooltip);
+        }
+    };
 
-    setInterval(window.foo, 100)
+    setTimeout(window.foo, 100)
+    // setInterval(window.foo, 100);
 
 }
 
 let patch_cache = false;
 const patch_fix = (geojson) => {
 
-    console.log(geojson.features.length)
+    console.log(geojson.features.length);
 
     // reference features
     const features = geojson.features;
@@ -124,13 +134,13 @@ const patch_fix = (geojson) => {
     // start a new cache, based on this geojson
     const new_patch_cache = _.reduce(features, (cache, f) => {
 
-        if (! (f.geometry && f.geometry.coordinates.length)){
+        if (!(f.geometry && f.geometry.coordinates.length)) {
 
             debugger;
         }
 
         const idx = f.properties['FEATURE_ID'];
-        if (! cache[idx]) cache[idx] = [f];
+        if (!cache[idx]) cache[idx] = [f];
         else cache[idx].push(f);
         return cache;
     }, {});
@@ -141,7 +151,7 @@ const patch_fix = (geojson) => {
     // check this geojson against previous cache
     // to fill in missing features
     else {
-        for (let key in patch_cache){
+        for (let key in patch_cache) {
             if (!(key in new_patch_cache)) {
                 new_patch_cache[key] = patch_cache[key];
 
@@ -149,32 +159,41 @@ const patch_fix = (geojson) => {
                 features.push(...patch_cache[key]);
             }
         }
-        patch_cache = new_patch_cache
+
+        patch_cache = new_patch_cache;
     }
 
-    return geojson
+    return geojson;
 };
 
-function render(mapUrl, path, svg){
-    d3.json(mapUrl, function(error, world) { 
-        world = patch_fix(world);
+function render(mapUrl, path, svg, tooltip) {
+    d3.json(mapUrl, function(error, world) {
 
-        // console.log(world.features[0].properties.TIME)
+        // world = patch_fix(world);
+
+        let year = world.features[0].properties.TIME;
+        document.getElementById('year').innerHTML = year;
 
         // remove all features
-        d3.selectAll('path').remove()
+        d3.selectAll('path').remove();
 
-        const data = svg.selectAll("path")
-            .data(world.features)
-
+        const data = svg.selectAll('path')
+            .data(world.features);
 
         data.enter()
-            .append("path")
-            .attr("class", "feature")
-            .style("fill", "white")
-            .style("stroke", 'grey')
-            .attr("d", path)
-
+            .append('path')
+            .attr('class', 'feature')
+            .style('fill', 'white')
+            .style('stroke', 'grey')
+            .attr('d', path)
+            .attr('name', path)
+            .on('mouseover', (d) => {
+                const rect = d3.event.target.getBoundingClientRect()
+                tooltip.text(d.properties['NAME'])
+                    .style('top', `${Math.floor(rect.top + rect.height/2)}px`)
+                    .style('left', `${Math.floor(rect.left + rect.width/2)}px`)
+                    .style("visibility", "visible")
+            })
 
     });
 
